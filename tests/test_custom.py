1644b06a624a55b3361fc2d53c23f54841a15873
jcalbrecht <jcalbrecht85@gmail.com>
2021-09-29 14:43:39 -0500

    add custom tests, update readme

--- tests/test_custom.py
+++ tests/test_custom.py
@@ -1,7 +1,6 @@
-import brownie
 from brownie import *
-from helpers.constants import AddressZero, MaxUint256
-from helpers.time import hours
+import brownie
+from helpers.constants import AddressZero
 
 """
   TODO: Put your tests here to prove the strat is good!
@@ -10,5 +9,29 @@ from helpers.time import hours
 """
 
 
-def test_my_custom_test():
-    assert True
+
+def test_price_feed_change(strategy, strategist, interface):
+
+  wbtc_feed = "0x6ce185860a4963106506C203335A2910413708e9"
+
+  with brownie.reverts("onlyGovernanceOrStrategist"):
+    strategy.setPriceFeeds(wbtc_feed, AddressZero, {'from': accounts[1]})
+
+  strategy.setPriceFeeds(wbtc_feed, AddressZero, {'from': strategist})
+
+  feed_from_strat = strategy.WBTC_PRICE_FEED()
+
+  assert feed_from_strat == wbtc_feed
+
+  rate = interface.AggregatorV2V3Interface(wbtc_feed).latestAnswer()
+  assert rate != None
+
+
+def test_set_gauge(strategy, strategist):
+  before_bal = strategy.balanceOfPool()
+  gauge = "0x97E2768e8E73511cA874545DC5Ff8067eB19B787"
+  with brownie.reverts("onlyGovernanceOrStrategist"):
+    strategy.setGauge(gauge, {'from': accounts[1]})
+
+  strategy.setGauge(gauge, {'from': strategist})
+
